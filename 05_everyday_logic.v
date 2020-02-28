4d29a17430e9068cb8b1a0e3f36e39b303050b07
Ayman Osman <aymano.osman@gmail.com>
2020-02-28 22:55:13 +0000

    solution of exercise 5.3

--- 05_everyday_logic.v
+++ 05_everyday_logic.v
@@ -2,25 +2,42 @@
 
 Lemma ex1 : ~False.
 Proof.
-  Admitted.
+  intro H.
+  assumption.
 Qed.
 
 Theorem ex2 : forall P:Prop, ~~~P->~P.
 Proof.
-  Admitted.
+  intros P H.
+  intro p.
+  apply H.
+  intro q.
+  apply q.
+  assumption.
 Qed.
 
 Theorem ex3 : forall P Q: Prop, ~~~P->P->Q.
 Proof.
-  Admitted.
+  intros P Q H p.
+  elim H.
+  intro H0.
+  apply H0.
+  assumption.
 Qed.
 
 Theorem ex4 : forall P Q: Prop, (P -> Q) -> ~Q -> ~P.
 Proof.
-  Admitted.
+  intros P Q H H0 p.
+  apply H0.
+  apply H.
+  assumption.
 Qed.
 
 Theorem ex5 : forall P Q R: Prop, (P->Q)->(P->~Q)->P->R.
 Proof.
-  Admitted.
+  intros P Q R H H0 p.
+  elim H0.
+  assumption.
+  apply H.
+  assumption.
 Qed.
