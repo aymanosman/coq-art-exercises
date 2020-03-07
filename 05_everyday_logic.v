c7c8ec164903c5fdb3e13a28d9d1b92f22d7b204
Ayman Osman <aymano.osman@gmail.com>
2020-03-07 15:00:14 +0000

    refactor exercise 5.3

--- 05_everyday_logic.v
+++ 05_everyday_logic.v
@@ -99,6 +99,9 @@ Proof.
 Qed.
 
 (* Exercise 5.3 *)
+(* Find the proofs that can be done without `False_ind`. Show how the
+   corresponding theorems can be seen as simple applications of more general
+   theorems from minimal propositional logic.*)
 
 Lemma ex1 : ~False.
 Proof.
@@ -106,26 +109,26 @@ Proof.
   assumption.
 Qed.
 
-Theorem ex2 : forall P:Prop, ~~~P->~P.
+Theorem ex2 : forall P : Prop, ~~~P -> ~P.
 Proof.
-  intros P H.
-  intro p.
+  intros P H p.
   apply H.
-  intro q.
-  apply q.
-  assumption.
-Qed.
-
-Theorem ex3 : forall P Q: Prop, ~~~P->P->Q.
-Proof.
-  intros P Q H p.
-  elim H.
   intro H0.
   apply H0.
   assumption.
 Qed.
 
-Theorem ex4 : forall P Q: Prop, (P -> Q) -> ~Q -> ~P.
+Theorem ex3 : forall P Q : Prop, ~~~P -> P -> Q.
+Proof.
+  intros P Q H p.
+  apply False_ind.
+  apply H.
+  intro H0.
+  apply H0.
+  assumption.
+Qed.
+
+Theorem ex4 : forall P Q : Prop, (P -> Q) -> ~Q -> ~P.
 Proof.
   intros P Q H H0 p.
   apply H0.
@@ -133,10 +136,11 @@ Proof.
   assumption.
 Qed.
 
-Theorem ex5 : forall P Q R: Prop, (P->Q)->(P->~Q)->P->R.
+Theorem ex5 : forall P Q R : Prop, (P -> Q) -> (P -> ~Q) -> P -> R.
 Proof.
   intros P Q R H H0 p.
-  elim H0.
+  apply False_ind.
+  apply H0.
   assumption.
   apply H.
   assumption.
