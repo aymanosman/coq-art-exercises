65641762b40a556b3077787ca6453abbec09631c
Ayman Osman <aymano.osman@gmail.com>
2020-03-07 14:07:47 +0000

    solutions of chapter 4 exercises done properly

--- 04_dependent_products.v
+++ 04_dependent_products.v
@@ -1,46 +1,29 @@
 (* Exercise 4.3 *)
 
 Section A_declared.
-  Variables (A:Set)(P Q:A->Prop)(R:A->A->Prop).
+  Variables (A : Set) (P Q : A -> Prop) (R : A -> A -> Prop).
 
-  Theorem all_perm : (forall a b:A, R a b) -> forall a b:A, R b a.
-  Proof.
-    intros H a b.
-    apply H.
-  Qed.
+  Theorem all_perm : (forall a b : A, R a b) -> forall a b : A, R b a.
+  Proof fun H a b => H b a.
 
-  Theorem all_imp_dist : (forall a:A, P a -> Q a) -> (forall a:A, P a) -> forall a:A, Q a.
-  Proof.
-    intros H H0 a.
-    apply H.
-    apply H0.
-  Qed.
+  Theorem all_imp_dist : (forall a : A, P a -> Q a) -> (forall a : A, P a) -> forall a : A, Q a.
+  Proof fun H H0 a => H a (H0 a).
 
-
-  Theorem all_delta : (forall a b:A, R a b)->forall a:A, R a a.
-  Proof.
-    intros H a.
-    apply H.
-  Qed.
+  Theorem all_delta : (forall a b : A, R a b) -> forall a : A, R a a.
+  Proof fun H a => H a a.
 
 End A_declared.
 
 (* Exercise 4.5 *)
 
-Theorem all_perm' : forall (A:Type) (P:A->A->Prop),
-    (forall x y: A, P x y)
-    -> forall x y:A, P y x.
-Proof.
-  intros A P H x y.
-  apply H.
-Qed.
+Theorem all_perm' : forall (A : Type) (P : A -> A -> Prop),
+    (forall x y : A, P x y)
+    -> forall x y : A, P y x.
+Proof fun A P H x y => H y x.
 
-Theorem resolution :
-  forall (A:Type)(P Q R S:A->Prop),
-    (forall a:A, Q a -> R a -> S a)
-    -> (forall b:A, P b -> Q b)
-    -> (forall c:A, P c -> R c -> S c).
-Proof.
-  intros A P Q R S H H0 c Pc Rc.
-  apply H;[apply H0; assumption | assumption].
-Qed.
+Theorem resolution : forall (A : Type) (P Q R S : A -> Prop),
+    (forall a : A, Q a -> R a -> S a)
+    -> (forall b : A, P b -> Q b)
+    -> (forall c : A, P c -> R c -> S c).
+Proof fun A P Q R S H H0 c pc rc =>
+        H c (H0 c pc) rc .
