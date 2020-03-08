57338cff0665c1b137252bd49ff9bd854d1404cd
Ayman Osman <aymano.osman@gmail.com>
2020-03-08 02:58:13 +0000

    solution of exercise 5.5

--- 05_everyday_logic.v
+++ 05_everyday_logic.v
@@ -176,3 +176,15 @@ Proof.
   assumption.
   apply I.
 Qed.
+
+(* Exercise 5.5 *)
+
+Theorem ex5_5 : forall (A: Set) (a b c d : A), a=c \/ b=c \/ c=c \/ d=c.
+Proof.
+  intros A a b c d.
+  apply or_intror.
+  apply or_intror.
+  apply or_introl.
+  apply eq_refl.
+Qed.
+
